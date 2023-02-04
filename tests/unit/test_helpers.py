4dcbfad2e43b72343a000c021b101b784e0c9650
Armando Yanez <ayanez20@wou.edu>
2023-02-04 14:12:31 -0800

    created test_helpers testing file to test helpers file and added tests that where included in the video and also added a test to test for 10 seconds ago

---
+++ tests/unit/test_helpers.py
@@ -0,0 +1,14 @@
+from app.helpers import pretty_date
+from datetime import datetime, timedelta
+
+
+def test_now():
+    assert pretty_date(datetime.utcnow()) == "just now"
+
+
+def test_seconds_ago():
+    assert pretty_date(datetime.utcnow() - timedelta(seconds=10)) == "10 seconds ago"
+
+
+def test_yesterday():
+    assert (pretty_date(datetime.utcnow() - timedelta(days=1))) == "Yesterday"
